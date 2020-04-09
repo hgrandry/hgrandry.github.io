6082e282f1d2f9a7c33ddfb32a997890dc6f7b94
Hadrien Grandry <hadrien.grandry@gmail.com>
2020-04-08 21:45:52 -0400

    initial commit

---
+++ precache-manifest.5c9b442a046651ad28ba1609e8f8001b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecfa4c3361fc17b4391d0d3e583d9ef1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3cf6b1df93b13440947d",
+    "url": "/portfolio/static/js/2.81b69b3e.chunk.js"
+  },
+  {
+    "revision": "321749a792540e3cc0b6581ec82f096f",
+    "url": "/portfolio/static/js/2.81b69b3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ccca45f4ac6148673bf",
+    "url": "/portfolio/static/js/main.f1fc1175.chunk.js"
+  },
+  {
+    "revision": "5e421121dae242743fda",
+    "url": "/portfolio/static/js/runtime-main.d17fafe0.js"
+  },
+  {
+    "revision": "af3793ff32565b15f099c7a4a04a01c8",
+    "url": "/portfolio/static/media/csharp.af3793ff.png"
+  },
+  {
+    "revision": "a6a3c0fa8138ac5246e318f128143eb6",
+    "url": "/portfolio/static/media/dotnet.a6a3c0fa.png"
+  },
+  {
+    "revision": "37fe8322b169ddbdeabf75930e886ac6",
+    "url": "/portfolio/static/media/react.37fe8322.png"
+  },
+  {
+    "revision": "26cc95f255ccb936d154b43614f61602",
+    "url": "/portfolio/static/media/typescript.26cc95f2.png"
+  },
+  {
+    "revision": "3bd509086b3dc061d478e47df5cdafbf",
+    "url": "/portfolio/static/media/unity.3bd50908.png"
+  }
+]);
